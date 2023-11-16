db81f0be5cc9cf748a9ebfb1943efebed17a8cd2
George Fincher <george@grimfunky.dev>
2023-11-16 00:07:46 +0000

    Feat ✨: Complete interfaces exercise

--- 6-type-defs-structs-interfaces/data/course.go
+++ 6-type-defs-structs-interfaces/data/course.go
@@ -13,6 +13,10 @@ type Course struct {
 	Instructor Instructor
 }
 
+func (c Course) SignUp() bool {
+	return true
+}
+
 func (c Course) String() string {
 	return fmt.Sprintf("--- %v --- (%v)\n", c.Name, c.Instructor.FirstName)
 }
--- 6-type-defs-structs-interfaces/main.go
+++ 6-type-defs-structs-interfaces/main.go
@@ -14,16 +14,24 @@ func main() {
 	max.LastName = "Fabiano"
 	max.Score = 96
 
-	kyle := data.NewInstructor("Kyle", "Simpson")
+	// kyle := data.NewInstructor("Kyle", "Simpson")
 
 	goCourse := data.Course{Id: 2, Name: "Go Fundamentals", Instructor: max}
 	
 	fmt.Println(max.Print())
-	fmt.Println(kyle.Print())
+	// fmt.Println(kyle.Print())
 	fmt.Println(goCourse.String())
 
 	// Swift workshop for embedding
 	swiftWS := data.NewWorkshop("Swift with iOS", max)
 
 	fmt.Printf("%v", swiftWS)
+
+	var courses [2]data.Signable
+	courses[0] = goCourse
+	courses[1] = swiftWS
+
+	for _, course := range courses {
+		fmt.Println(course)
+	}
 }
